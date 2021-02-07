11b1cdfa2b0c0e35543d2e95aac8e6fadc1b8123
RAO <raorao1011@gmail.com>
2021-02-07 13:58:23 +0900

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+"use strict";
+
+{
+    function setWord() {
+        word = words.splice(Math.floor(Math.random() * words.length), 1)[0]; //ランダムな位置から一個削除
+        // spliceの返り値は一個でも配列になるから添え字で0を加える
+        target.textContent = word;
+        loc = 0;
+    }
+
+    const words = [
+        "red",
+        "blue",
+        "pink",
+    ];
+    let word;
+    let loc = 0;
+
+    const target = document.getElementById("target");
+
+    document.addEventListener("click", () => {
+        setWord();
+    });
+
+    document.addEventListener("keydown", e => {
+        if (e.key !== word[loc]) {
+            return;
+        };
+
+        loc++;
+
+        //1: _ed
+        //2: __d
+        //3: ___
+        target.textContent = "_".repeat(loc) + word.substring(loc);
+
+        if (loc === word.length) {
+            if (words.length === 0) {
+                const result = document.getElementById("result");
+                result.textContent = "Finished!";
+            }
+            setWord();
+        }
+    });
+}
