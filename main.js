87f93396271edbd3045d88f1ee9ca7e8a7a400c8
RAO <raorao1011@gmail.com>
2021-02-07 14:07:55 +0900

    complete

--- main.js
+++ main.js
@@ -15,10 +15,17 @@
     ];
     let word;
     let loc = 0;
+    let startTime;
+    let isPlaying = false;
 
     const target = document.getElementById("target");
 
     document.addEventListener("click", () => {
+        if (isPlaying === true) {
+            retrun;
+        }
+        isPlaying = true;
+        startTime = Date.now();
         setWord();
     });
 
@@ -36,8 +43,10 @@
 
         if (loc === word.length) {
             if (words.length === 0) {
+                const elapsedTime = ((Date.now() - startTime) / 1000).toFixed(2);
                 const result = document.getElementById("result");
-                result.textContent = "Finished!";
+                result.textContent = `Finished! ${elapsedTime} seconds`;
+                return;
             }
             setWord();
         }
